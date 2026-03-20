ab5e0365358998608e22f0b7c50b5988f4755b46
HarshSharma-0 <aparna6008in@gmail.com>
2026-03-20 17:15:51 +0530

    minor changes

--- cursor.h
+++ cursor.h
@@ -5,73 +5,50 @@
 
 namespace vt100 {
 
-    // ----- Cursor Movement -----
+// ----- Cursor Movement -----
 
-    inline void cursorUp(int n = 1) {
-        std::cout << "\033[" << n << "A";
-    }
+inline void cursorUp(int n = 1) { std::cout << "\033[" << n << "A"; }
 
-    inline void cursorDown(int n = 1) {
-        std::cout << "\033[" << n << "B";
-    }
+inline void cursorDown(int n = 1) { std::cout << "\033[" << n << "B"; }
 
-    inline void cursorForward(int n = 1) {
-        std::cout << "\033[" << n << "C";
-    }
+inline void cursorForward(int n = 1) { std::cout << "\033[" << n << "C"; }
 
-    inline void cursorBack(int n = 1) {
-        std::cout << "\033[" << n << "D";
-    }
+inline void cursorBack(int n = 1) { std::cout << "\033[" << n << "D"; }
 
-    inline void cursorMoveTo(int row, int col) {
-        std::cout << "\033[" << row << ";" << col << "H";
-    }
-
-    // ----- Line Clearing -----
-
-    inline void clearLine() {
-        // Clear entire line
-        std::cout << "\033[2K";
-    }
-
-    inline void clearLineToRight() {
-        std::cout << "\033[0K";
-    }
-
-    inline void clearLineToLeft() {
-        std::cout << "\033[1K";
-    }
-
-    inline void clearScreen() {
-        std::cout << "\033[2J";
-    }
-
-    inline void clearScreenAndHome() {
-        std::cout << "\033[2J\033[H";
-    }
-
-    // ----- Cursor visibility -----
-
-    inline void hideCursor() {
-        std::cout << "\033[?25l";
-    }
-
-    inline void showCursor() {
-        std::cout << "\033[?25h";
-    }
-    inline void flush(){
-       std::cout.flush();
-    };
-    inline void moveUpAndClearSingleLine(int n) {
-      std::cout << "\033[" << n << "A"; // Move up
-      std::cout << "\033[2K";           // Clear entire current line
-    };
-
-    inline void moveUpAndClearBelow(int n) {
-      std::cout << "\033[" << n << "A";
-      std::cout << "\033[0J";
-   };
+inline void cursorMoveTo(int row, int col) {
+  std::cout << "\033[" << row << ";" << col << "H";
 }
 
-#endif // VT100_HPP
+// ----- Line Clearing -----
 
+inline void clearLine() {
+  // Clear entire line
+  std::cout << "\033[2K";
+}
+
+inline void clearLineToRight() { std::cout << "\033[0K"; }
+
+inline void clearLineToLeft() { std::cout << "\033[1K"; }
+
+inline void clearScreen() { std::cout << "\033[2J"; }
+
+inline void clearScreenAndHome() { std::cout << "\033[2J\033[H"; }
+
+// ----- Cursor visibility -----
+
+inline void hideCursor() { std::cout << "\033[?25l"; }
+
+inline void showCursor() { std::cout << "\033[?25h"; }
+inline void flush() { std::cout.flush(); };
+inline void moveUpAndClearSingleLine(int n) {
+  std::cout << "\033[" << n << "A"; // Move up
+  std::cout << "\033[2K";           // Clear entire current line
+};
+
+inline void moveUpAndClearBelow(int n) {
+  std::cout << "\033[" << n << "A";
+  std::cout << "\033[0J";
+};
+} // namespace vt100
+
+#endif // VT100_HPP
